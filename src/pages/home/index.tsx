5b3999125b1fd202c1edf7d6fedbe6ea8f131e77
Sk4rpy <64379275+Sk4rpy@users.noreply.github.com>
2024-03-31 04:51:42 +0300

    Card entity

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,5 @@
 import { Code } from "@chakra-ui/react";
 
 export default function Index() {
-  return <Code>(FSD) Remix-Vite + Chakra template</Code>;
+    return <Code>(FSD) Remix-Vite + Chakra template</Code>;
 }
